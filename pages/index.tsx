21a2dc2d5a67e376de5640528b8e4f41be25f726
andreazanottoits <azanotto80@gmail.com>
2022-08-04 17:36:57 +0200

    Add api data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,44 @@
-import type { NextPage } from "next";
+import { PokemonCard } from "../components/pokemonCard";
+import { ApiAllPokemonResponse, Pokemon } from "../types/types";
 
-const Home: NextPage = () => {
+export async function getServerSideProps(): Promise<{
+  props: { pokemons: ApiAllPokemonResponse };
+}> {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+  const pokemonsFetched: ApiAllPokemonResponse = await res.json();
+
+  const pokemonDetail = await Promise.all(
+    pokemonsFetched.results.map((element: Pokemon) =>
+      fetch(`https://pokeapi.co/api/v2/pokemon/` + element.name).then(
+        (response) => response.json()
+      )
+    )
+  );
+  for (let pokemonFetched of pokemonsFetched.results) {
+    pokemonFetched.image = pokemonDetail.find(
+      (element: Pokemon) => element.name === pokemonFetched.name
+    ).sprites.front_default;
+  }
+
+  return {
+    props: {
+      pokemons: pokemonsFetched,
+    },
+  };
+}
+
+const Home = ({ pokemons }: any) => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <div className="flex justify-center flex-row">
+        <h1 className="text-5xl font-bold mt-5 mb-4">Pokemon list</h1>
+      </div>
+      <div className="grid grid-cols-4 gap-4 m-3">
+        {pokemons.results.map((pokemon: Pokemon, index: number) => (
+          <PokemonCard key={index} name={pokemon.name} image={pokemon.image} />
+        ))}
+      </div>
+    </>
   );
 };
 
